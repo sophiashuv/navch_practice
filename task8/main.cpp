252a4cc7987ec40cc3b083c81378a58845b7f93a
Sophia Shuvar <45092554+sophiashuv@users.noreply.github.com>
2021-05-06 10:46:09 +0300

    Update main.cpp

--- task8/main.cpp
+++ task8/main.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include "Polynomial.h"
 
 using namespace std;
 
-const vector<char> vowels = { 'a', 'o', 'u', 'e', 'i', 'y' };
+int main() {
+    Polynomial a, b;
+    cin >> a >> b;
 
-vector<string> input_text() {
-    cout << "Enter text: ";
-    vector<string> text = vector<string>();
-    string word = " ";
-    while (word[word.length() - 1] != '.') {
-        cin >> word;
-        text.push_back(word);
+    cout << "a: " << a << "\n";
+    cout << "b: " << b << "\n";
+    cout << "sum(a, b): " << a + b << "\n";
+
+    int aa, bb, h;
+    cout << "Enter interval: \na = ";
+    cin >> aa;
+    cout << "b = ";
+    cin >> bb ;
+    cout << "Enter h: \nh = ";
+    cin >> h;
+    for (int i = aa; i < bb; i += h) {
+        cout << "x = " << i << ": f1(x) = " << a.evaluate(i) << "\n";
+        cout << "x = " << i << ": f2(x) = " << b.evaluate(i) << "\n";
     }
 
-    auto last_word = text[text.size() - 1];
-    last_word.pop_back();
-    text[text.size() - 1] = last_word;
-    return text;
-}
-
-void count_vovels_percentage(string word) {
-    int num_vowels = count_if(word.begin(), word.end(),
-        [&](char i) {return find(vowels.begin(), vowels.end(), i) != vowels.end(); });
-
-    double percentage = (double)num_vowels * 100 / word.length();
-    cout << word << " : " << percentage << "%\n";
-}
-
-int main() {
-    vector<string> text = input_text();
-    for_each(text.begin(), text.end(), [&](string word) {count_vovels_percentage(word); });
-    return 0;
-}
+    int x;
+    cout << "Enter x: ";
+    cin >> x;
+    if (a == b) {
+        cout << "x = " << x << " : f(x) = " << a.evaluate(x) << "\n";
+    }
+    else {
+        cout << "a * x: " << a * x << "\n";
+        cout << "b': " << b.differentiate() << "\n";
+    }
+} 
